d05a14774dc15606ebf45b7e160cb9e4332e39a9
ShAhilK2 <shahilkataria852002@gmail.com>
2025-10-16 22:13:34 +0530

    02 : Database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import prisma from "@/lib/db";
+import React, { use } from "react";
 
-const Page = () => {
-  return <div className="text-red-400">HELLO WORLD</div>;
+const Page = async () => {
+  const user = await prisma.user.findMany();
+  return (
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      {JSON.stringify(user)}
+    </div>
+  );
 };
 
 export default Page;
